24f74c0c70ceed8aebfcb67e6c53f81c5d61e32e
JulianoF <juliano1@live.ca>
2024-03-14 19:12:31 -0400

    Get Spring Project Setup

---
+++ TicketApplication/src/test/java/net/group18/TicketApplication/TicketApplicationTests.java
@@ -0,0 +1,13 @@
+package net.group18.TicketApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
