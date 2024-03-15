92ee15baf1abb3be9fb369360cf4f4afc81bfe1f
JulianoF <juliano1@live.ca>
2024-03-15 14:51:56 -0400

    Work on Structure

---
+++ TicketApplication/src/main/java/net/group18/TicketApplication/HomePageController.java
@@ -0,0 +1,18 @@
+package net.group18.TicketApplication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomePageController {
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+}
